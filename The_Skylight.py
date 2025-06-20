8874bc60e2b3930b39d734ed430b2de8c3b196e8
Jordan Ling <anotherjordanishere@gmail.com>
2025-06-21 00:50:45 +0800

    shuffle implemented

--- The_Skylight.py
+++ The_Skylight.py
@@ -11,9 +11,9 @@ settings = {"debug" : True,
             "paused" : False,
             "num_songs" : 0,
             "count" : 0,
-            "song_names" : [],
+            "song_names" : [], #to be phased out
             "song_dict" : {} } #queue order, (name, path)
-song_queue = {}
+# song_queue = {}
 
 def bounds_check():
     if settings["count"] >= settings["num_songs"]:
@@ -48,7 +48,7 @@ async def user_conts():
             settings["paused"] = False
             settings["count"] -= 2
             bounds_check()
-        elif cmd == "Q": # quit player
+        elif cmd == "Q" or cmd == "quit": # quit player
             pygame.mixer.music.stop()
             print("Quitting player.")
             os._exit(0)
@@ -62,35 +62,70 @@ async def user_conts():
         elif cmd == "Caddy and a Sunflower":
             print("The blind eyes turned, the excuses made, the insidious lies whispered into the ear of a child")
         elif cmd == '>': #whats next
+            bounds_check()
             count = settings["count"]
             if count == settings["num_songs"] - 1:
                 count = - 1
+                if settings["debug"]:
+                    print(count)
             print("Playing next: " + ((settings["song_dict"])[count + 1])[0])
-
         elif cmd == '<':
+            bounds_check()
+            if settings["count"] == -1:
+                print("You just shuffled the playlist, unable to display previous song") #fix at a later date
+                continue
             count = settings["count"]
-            print(count)
+            if settings["debug"]:
+                print(count)
             if count == 0:
                 count = settings["num_songs"]
-                print(count)
-            
+                if settings["debug"]:
+                    print(count)
             print("Played previously: " + ((settings["song_dict"])[count - 1])[0])
         elif cmd == 'n':
+            if settings["count"] == -1:
+                print("You just shuffled the playlist, unable to display previous song") #fix at a later date
+                continue
             print("Playing now: " + ((settings["song_dict"])[settings["count"]])[0])
         elif cmd == 'queue' or cmd == 'q':
             queue_count = settings["count"]
             # list_count = 1
-            print(f"    Playing now: {(settings['song_names'])[queue_count]}") #playing now
+            print(f"    Playing now: {((settings["song_dict"])[settings["count"]])[0]}") #playing now
             queue_count += 1
             for i in range(1, settings["num_songs"]):
                 if queue_count >= settings["num_songs"]:
                     queue_count = queue_count - settings["num_songs"]
-                print(f"    {i}. {(settings['song_names'])[queue_count]}")
+                print(f"    {i}. {((settings["song_dict"])[queue_count])[0]}")
                 queue_count += 1
                 # list_count += 1
         elif cmd == 'shuffle' or cmd == 's':
             #in development
-            print((settings["song_dict"]))
+            bounds_check()
+            song_list = list(settings["song_dict"].values())
+            if settings["debug"]:
+                print("song_list length: " + str(len(song_list)))
+            random.shuffle(song_list)
+            if settings["debug"]:
+                print(settings["num_songs"])
+                print(len([0, 1]))
+            for i in range(settings["num_songs"]):
+                if settings["debug"]:
+                    print("shuffle i = " + str(i))
+                (settings["song_dict"])[i] = song_list[i]
+                
+            queue_count = 1
+            print("New Shuffled Queue")
+            print(f"    Playing next: {((settings['song_dict'])[queue_count])[0]}")
+            queue_count += 1
+            for i in range(1, settings["num_songs"]):
+                if queue_count >= settings["num_songs"]:
+                    queue_count = queue_count - settings["num_songs"]
+                print(f"    {i + 1}. {((settings['song_dict'])[queue_count])[0]}")
+                queue_count += 1                
+            settings["count"] = 0
+            # if settings["debug"]:
+            #     print("Current count: " + str(settings["count"]))
+            #     print("Song at current count: " + str(((settings['song_dict'])[(settings["count"])])[0]))
             
             
             
@@ -102,7 +137,6 @@ async def main():
     queue_order = 0
     
     for song in songs: #song paths
-        settings["num_songs"]+=1
         decoded_song = unquote(song) 
         song_path = os.path.join('music', decoded_song)
         (settings["song_names"]).append(decoded_song)
@@ -110,6 +144,9 @@ async def main():
         
         (settings["song_dict"])[queue_order] = (decoded_song, song_path)
         queue_order += 1
+        settings["num_songs"]+=1
+        if settings["debug"]:
+            print("num_songs_main is: " + str(settings["num_songs"]))
         
     print("Queue: ")
     queue_count = 0
@@ -119,13 +156,14 @@ async def main():
     print()
     
     while True:
-        print(settings["num_songs"])
+        if settings["debug"]: 
+            print(settings["num_songs"])
         if settings["count"] >= settings["num_songs"]:
             # settings["count"] = settings["count"] - (settings["num_songs"])
             settings["count"] = (settings["count"]) % settings["num_songs"]
             
-        print("Now playing: " + (settings["song_names"])[settings["count"]])
-        await play_song(song_paths[settings["count"]])
+        print("Now playing: " + ((settings["song_dict"])[settings["count"]])[0])
+        await play_song(((settings["song_dict"])[settings["count"]])[1])
         if settings["debug"]:
             print("count is: " + str(settings["count"]))
             
